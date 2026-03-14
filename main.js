dec350dc7dbaa46562d490e228b8f6be88bc7a65
Napahwz <pilou26092005@gmail.com>
2026-03-14 15:33:11 +0100

    feat: Adding scene, camera & renderer

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import * as THREE from 'three';
+
+const canvaWidth = 900;
+const canvaHeight = 800;
+
+// Creation of scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(90, canvaWidth / canvaHeight, 0.1, 1000);
+
+// Creation of renderer and adding to the document body
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(canvaWidth,canvaHeight);
+document.body.appendChild(renderer.domElement);
